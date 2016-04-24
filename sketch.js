28086ca48ce5ffae8d86337008c3bcf06e67f301
clarkrosen <clark.rosen24@gmail.com>
2016-04-24 16:47:12 -0400

    fb share

--- sketch.js
+++ sketch.js
@@ -59,6 +59,12 @@ function setup() {
         cookie: true, 
         xfbml: true
     });    
+    
+   FB.ui({
+  method: 'share',
+  href: 'https://clarkrosen.github.io/index.html',
+}, function(response){});
+
 
 }
 
@@ -102,4 +108,11 @@ function mouseDragged() {
   	p.push( new Splash(windowWidth/2, mouseY) );
 
   }
-
+  
+ (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
